d784e735e5030416d477c43a03d5f31238dbdbe4
Mexta46 <22030043@itesa.edu.mx>
2024-08-18 15:05:44 -0600

    cambios 2

--- setup.py
+++ setup.py
@@ -7,12 +7,7 @@ setup(
     author='Martin',
     author_email='tu.email@example.com',
     description='Descripción de lo que hace tu librería',
-    long_description=open('Readme.md').read(),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
     python_requires='>=3.6',
 )
